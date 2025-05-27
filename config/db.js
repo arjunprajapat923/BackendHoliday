f856e80e1b5ea902552b5483cf941d2008b4265f
arjunprajapat923 <prajapatiarjun68663@gmail.com>
2025-05-27 14:23:35 +0530

    for deployment

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/yourdbname");
+    const conn = await mongoose.connect("mongodb+srv://prajapatiarjun68663:rvSwDZeIqo4vdnMg@cluster0.r4ebzwo.mongodb.net/");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const connectDB = require("../backend/config/db");
 const userRoutes = require("./routes/userRoutes");
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
